b65f618f82218642c0ce6479151e0e02b52d600e
Shobhit <shobhitagrawal2004@gmail.com>
2024-12-17 19:45:34 +0530

    Sum of 2 numbers

---
+++ Sum_2_num.java
@@ -0,0 +1,10 @@
+public class Sum_2_num {
+
+    public static void main(String[] args) {
+        int num1, num2, total;
+        num1 = Integer.parseInt(args[0]);
+        num2 = Integer.parseInt(args[1]);
+        total = num1 + num2;
+        System.out.println("Sum is " + total);
+    }
+}
